30182217d30b6bb86c8d986b3906bd8f01c1b4eb
lucastheldl <lucastheldl@gmail.com>
2022-11-16 01:36:11 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import {
+  BsTrash,
+  BsBookmarkCheck,
+  BsBookmarkCheckFill,
+  icons,
+} from "react-icons/bs";
+import Header from "./components/Header";
+
+const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //Load todos before on page load
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((er) => console.log(er));
+
+      setLoading(false);
+      setTodos(res);
+    };
+
+    loadData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) => [...prevState, todo]); //serve para que a tafera apareca assim que add
+
+    setTitle("");
+    setTime("");
+  };
+
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  };
+  const handleEdit = async (todo) => {
+    todo.done = !todo.done;
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) =>
+      prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    );
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <div className="form-todo">
+        <h2>Insira sua Tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">O que voce vai fazer?</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="titulo da tarefa"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+              value={title || ""}
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input
+              type="text"
+              name="time"
+              placeholder="tempo da tarefa(Horas)"
+              onChange={(e) => {
+                setTime(e.target.value);
+              }}
+              value={time || ""}
+              required
+            />
+          </div>
+
+          <input type="submit" value="Criar tarefa" />
+        </form>
+      </div>
+
+      <div className="list-todo">
+        <h2>Lista de Tarefas</h2>
+        {todos.length === 0 && <p>Sem Tarefas!</p>}
+
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Duração:{todo.time}</p>
+            <div className="actions">
+              <span onClick={() => handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="todo-header">
+      <h1>React To do</h1>
+    </div>
+  );
+};
+
+export default Header;
